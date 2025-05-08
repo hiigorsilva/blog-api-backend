ac6ed2ded5d61cd2e3aed6775c0894b2794dcb96
hiigorsilva <higorscontato@gmail.com>
2025-05-08 15:19:39 -0300

    feat: create get post schema

--- src/modules/posts/post.schemas.ts
+++ src/modules/posts/post.schemas.ts
@@ -1,75 +1,18 @@
 import type { RouteShorthandOptions } from 'fastify/types/route'
 import z from 'zod'
 import { privateRoute } from '../../middlewares/private-route'
-import {
-  addPostBodySchema,
-  addPostResponseSchema,
-  authorizationHeaderSchema,
-  editPostBodySchema,
-  getAllPostsSchema,
-  queryPostSchema,
-  slugPostSchema,
-} from '../../schema/post'
+import * as post from '../../schema/post'
 
-export const addPost: RouteShorthandOptions = {
+export const getPost: RouteShorthandOptions = {
   preHandler: [privateRoute],
   schema: {
-    summary: 'Create a new post',
+    summary: 'Get a post by slug',
     consumes: ['application/json'],
     tags: ['Posts'],
-    headers: z.object({
-      authorization: z.string(),
-    }),
-    body: addPostBodySchema,
+    params: post.slugPostSchema,
+    headers: post.authorizationHeaderSchema,
     response: {
-      201: addPostResponseSchema,
-      400: z.object({
-        error: z.string(),
-        message: z.string(),
-      }),
-      401: z.object({ error: z.string() }),
-      500: z.object({ error: z.string() }),
-    },
-  },
-}
-
-export const editPost: RouteShorthandOptions = {
-  preHandler: [privateRoute],
-  schema: {
-    summary: 'Edit a post by slug',
-    consumes: ['application/json'],
-    tags: ['Posts'],
-    params: slugPostSchema,
-    headers: authorizationHeaderSchema,
-    body: editPostBodySchema,
-    response: {
-      201: addPostResponseSchema,
-      400: z.object({
-        error: z.string(),
-        message: z.string(),
-      }),
-      401: z.object({ error: z.string() }),
-    },
-  },
-}
-
-export const removePost: RouteShorthandOptions = {
-  preHandler: [privateRoute],
-  schema: {
-    summary: 'Remove a post by slug',
-    consumes: ['application/json'],
-    tags: ['Posts'],
-    params: slugPostSchema,
-    headers: authorizationHeaderSchema,
-    response: {
-      200: z.object({
-        message: z.string(),
-        post: z.object({
-          postId: z.string(),
-          title: z.string(),
-          authorName: z.string(),
-        }),
-      }),
+      200: post.getPostSchema,
       400: z.object({
         error: z.string(),
         message: z.string(),
@@ -87,15 +30,85 @@ export const getPosts: RouteShorthandOptions = {
     summary: 'Get all posts',
     consumes: ['application/json'],
     tags: ['Posts'],
-    querystring: queryPostSchema,
-    headers: authorizationHeaderSchema,
+    querystring: post.queryPostSchema,
+    headers: post.authorizationHeaderSchema,
     response: {
-      200: getAllPostsSchema,
+      200: post.getAllPostsSchema,
       400: z.object({
         error: z.string(),
         message: z.string(),
       }),
       401: z.object({ error: z.string() }),
+      500: z.object({ error: z.string() }),
+    },
+  },
+}
+
+export const addPost: RouteShorthandOptions = {
+  preHandler: [privateRoute],
+  schema: {
+    summary: 'Create a new post',
+    consumes: ['application/json'],
+    tags: ['Posts'],
+    headers: z.object({
+      authorization: z.string(),
+    }),
+    body: post.addPostBodySchema,
+    response: {
+      201: post.addPostResponseSchema,
+      400: z.object({
+        error: z.string(),
+        message: z.string(),
+      }),
+      401: z.object({ error: z.string() }),
+      500: z.object({ error: z.string() }),
+    },
+  },
+}
+
+export const editPost: RouteShorthandOptions = {
+  preHandler: [privateRoute],
+  schema: {
+    summary: 'Edit a post by slug',
+    consumes: ['application/json'],
+    tags: ['Posts'],
+    params: post.slugPostSchema,
+    headers: post.authorizationHeaderSchema,
+    body: post.editPostBodySchema,
+    response: {
+      201: post.addPostResponseSchema,
+      400: z.object({
+        error: z.string(),
+        message: z.string(),
+      }),
+      401: z.object({ error: z.string() }),
+    },
+  },
+}
+
+export const removePost: RouteShorthandOptions = {
+  preHandler: [privateRoute],
+  schema: {
+    summary: 'Remove a post by slug',
+    consumes: ['application/json'],
+    tags: ['Posts'],
+    params: post.slugPostSchema,
+    headers: post.authorizationHeaderSchema,
+    response: {
+      200: z.object({
+        message: z.string(),
+        post: z.object({
+          postId: z.string(),
+          title: z.string(),
+          authorName: z.string(),
+        }),
+      }),
+      400: z.object({
+        error: z.string(),
+        message: z.string(),
+      }),
+      401: z.object({ error: z.string() }),
+      404: z.object({ error: z.string() }),
       500: z.object({ error: z.string() }),
     },
   },
