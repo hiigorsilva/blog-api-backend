9a5e448b2e171d459dc95fc9b0f0aab0ae7c1a18
hiigorsilva <higorscontato@gmail.com>
2025-05-08 16:09:55 -0300

    feat: create published post controller

--- src/modules/main/main.controllers.ts
+++ src/modules/main/main.controllers.ts
@@ -1,4 +1,7 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
+import { queryPostSchema } from '../../schema/post'
+import type { GetPublishedPostsResponseType } from '../../types/posts/post'
+import { getAllPublishedPosts } from '../posts/post.services'
 
 export const ping = async (request: FastifyRequest, reply: FastifyReply) => {
   const { ip } = request
@@ -7,4 +10,43 @@ export const ping = async (request: FastifyRequest, reply: FastifyReply) => {
   return reply.status(200).send({
     ping: 'pong',
   })
+}
+
+export const getPublishedPosts = async (
+  request: FastifyRequest,
+  reply: FastifyReply
+) => {
+  const pageQuery = queryPostSchema.safeParse(request.query)
+  if (!pageQuery.success) {
+    return reply.status(400).send({
+      error: pageQuery.error.flatten().fieldErrors,
+      message: pageQuery.error.message,
+    })
+  }
+
+  if (pageQuery.data.page <= 0) {
+    return reply.status(404).send({ error: 'Page not found' })
+  }
+
+  const { page } = pageQuery.data
+
+  const posts = await getAllPublishedPosts(page)
+  if (!posts) {
+    return reply.status(500).send({ error: 'Failed to get posts' })
+  }
+
+  const publishedPosts: GetPublishedPostsResponseType = {
+    posts: posts.map(post => ({
+      id: post.authorId,
+      title: post.title,
+      slug: post.slug,
+      tags: post.tags,
+      authorName: post.author.name,
+      updatedAt: post.updatedAt,
+      createdAt: post.createdAt,
+    })),
+    page: page,
+  }
+
+  return reply.status(200).send(publishedPosts)
 }
